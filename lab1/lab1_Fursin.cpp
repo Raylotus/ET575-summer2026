17224949186bfb42200b5b1c18b250a448416ac6
Raylotus <163777611+Raylotus@users.noreply.github.com>
2026-05-29 01:04:17 +0000

    completed Lab 1

--- lab1/lab1_Fursin.cpp
+++ lab1/lab1_Fursin.cpp
@@ -9,6 +9,42 @@ using namespace std;
 
 int main(){
 
-    cout<<"Hello World!";
+    cout<<"Hello World!\n"<<endl;
+    cout<<"ET 575 \t\t Maksym Fursin"<<endl;
+    cout<<"\n---------- Example 2: declaring string variables ----------"<<endl;
+
+    string username;
+    username = "Maksym Fursin";
+    cout<<"Username = "<<username<<endl;
+    cout<<"Enter another name = "<<endl;
+    cin>>username;
+    cout<<"Updated Username = "<<username<<endl;
+
+    cout<<"\n---------- Example 3: declaring character variables ----------"<<endl;
+
+    char symbol = '%';
+    cout<<"Character = "<<symbol<<endl;
+    symbol = 38;
+    cout<<"Character = "<<symbol<<endl;
+
+    char s(35);
+    cout<<"Symbol = "<<s<<endl;
+
+    cout<<"\n---------- EXERCISE ----------"<<endl;
+
+    string country;
+    char gender;
+
+    cout<<"Enter the name of the country -> ";
+    cin>>country;
+    cout<<"Select gender -> ";
+    cin>>gender;
+
+    cout<<"Entered country:\t"<<country<<endl;
+    cout<<"Selected gender:\t"<<gender<<endl;
+
+
+    
+
     return 0;
 }
